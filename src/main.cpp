bc978b86f91b393b67cf7225e8700f495b24eff4
Diego Santos Seabra <dss-diego@hotmail.com>
2022-02-01 23:24:07 -0300

    added vulkan lib / creating first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
+// TODO: Vulkan documentation :: Step 6 - Graphics Pipeline
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
 
