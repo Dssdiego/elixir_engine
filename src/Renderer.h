ccebc717a433d24ee19b5eb0a06a5090447a694a
Diego Santos Seabra <dss-diego@hotmail.com>
2022-03-06 12:31:32 -0300

    renderer class creation

---
+++ src/Renderer.h
@@ -0,0 +1,14 @@
+//
+// Created by Diego S. Seabra on 03/03/22.
+//
+
+#ifndef VULKAN_ENGINE_RENDERER_H
+#define VULKAN_ENGINE_RENDERER_H
+
+class Renderer
+{
+
+};
+
+
+#endif //VULKAN_ENGINE_RENDERER_H
