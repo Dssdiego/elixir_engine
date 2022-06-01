bb19bfeb3848a090c4e2cc9aa559c1af00ae78d7
Diego Santos Seabra <dss-diego@hotmail.com>
2022-06-01 00:21:04 -0300

    initial scene system (testing entt only for now)

---
+++ src/engine/scenes/SceneSystem.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Diego S. Seabra on 31/05/22.
+//
+
+#include "SceneSystem.h"
+
+void CSceneSystem::Init()
+{
+    entt::registry registry;
+}
+
+void CSceneSystem::Shutdown()
+{
+
+}
---
+++ src/engine/scenes/SceneSystem.h
@@ -0,0 +1,18 @@
+//
+// Created by Diego S. Seabra on 31/05/22.
+//
+
+#ifndef VULKAN_ENGINE_SCENESYSTEM_H
+#define VULKAN_ENGINE_SCENESYSTEM_H
+
+#include <entt/entt.hpp>
+
+class CSceneSystem
+{
+public:
+    static void Init();
+    static void Shutdown();
+};
+
+
+#endif //VULKAN_ENGINE_SCENESYSTEM_H
