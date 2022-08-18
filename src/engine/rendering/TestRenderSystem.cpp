a5ff6e19bd32f352bb91896fe93fa61e4245e650
Diego Santos Seabra <dss-diego@hotmail.com>
2022-08-17 23:57:31 -0300

    test render system (initial implementation)

---
+++ src/engine/rendering/TestRenderSystem.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Diego S. Seabra on 17/08/22.
+//
+
+#include "TestRenderSystem.h"
+#include "VulkanPipeline.h"
+#include "EngineRenderer.h"
+
+// TODO: Refactor the code so that we don't use raw pointers. Instead we want to use smart pointers
+//       See more here: https://stackoverflow.com/questions/106508/what-is-a-smart-pointer-and-when-should-i-use-one
+TestRenderSystemImpl* mTestRenderSystemImpl = nullptr;
+
+//
+// Initialization/Destruction
+//
+
+void TestRenderSystem::Init()
+{
+    mTestRenderSystemImpl = new TestRenderSystemImpl;
+}
+
+void TestRenderSystem::Shutdown()
+{
+    delete mTestRenderSystemImpl;
+}
+
+void TestRenderSystem::RenderGameObjects()
+{
+    // TODO: Continue here
+//    mTestRenderSystemImpl->RenderGameObjects();
+}
+
+//
+// Implementation
+//
+
+TestRenderSystemImpl::TestRenderSystemImpl()
+{
+    VulkanPipeline::Init();
+}
+
+TestRenderSystemImpl::~TestRenderSystemImpl()
+{
+    VulkanPipeline::Shutdown();
+}
+
+// REVIEW: Probably the render system knows too much about vulkan and pushing constants
+//          Perhaps this is a responsibility for the EngineRenderer?
+void TestRenderSystemImpl::RenderGameObjects(std::vector<int> &gameObjects)
+{
+    // TODO: Go to each object in the array and render it
+    for (auto &obj : gameObjects)
+    {
+        auto commandBuffer = EngineRenderer::GetCurrentCommandBuffer();
+
+        SimplePushConstantData push{};
+//        push.offset = obj.transform2d.translation;
+//        push.color = obj.color;
+//        push.transform = obj.transform2d.mat2();
+
+        vkCmdPushConstants(
+                commandBuffer,
+                nullptr,
+                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                0,
+                sizeof(SimplePushConstantData),
+                &push
+        );
+
+//        obj.model.bind();
+//        obj.model.draw();
+    }
+
+
+}
---
+++ src/engine/rendering/TestRenderSystem.h
@@ -0,0 +1,29 @@
+//
+// Created by Diego S. Seabra on 17/08/22.
+//
+
+#ifndef VULKAN_ENGINE_TESTRENDERSYSTEM_H
+#define VULKAN_ENGINE_TESTRENDERSYSTEM_H
+
+#include <iostream>
+#include <vector>
+
+struct TestRenderSystemImpl
+{
+    TestRenderSystemImpl();
+    ~TestRenderSystemImpl();
+
+    void RenderGameObjects(std::vector<int> &gameObjects);
+};
+
+class TestRenderSystem
+{
+public:
+    static void Init();
+    static void Shutdown();
+
+    static void RenderGameObjects();
+};
+
+
+#endif //VULKAN_ENGINE_TESTRENDERSYSTEM_H
