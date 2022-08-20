5fe830195e19487f7aa0e6b601898e8447907c67
Diego Santos Seabra <dss-diego@hotmail.com>
2022-08-20 02:25:42 -0300

    creating vertex buffer in the base shape class

--- src/engine/rendering/shapes/BaseShape.cpp
+++ src/engine/rendering/shapes/BaseShape.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "BaseShape.h"
+
+//
+// Public/External
+//
+
+void BaseShape::CreateShape()
+{
+    CreateVertexBuffers();
+}
+
+void BaseShape::Destroy()
+{
+    vkDestroyBuffer(VulkanDevice::GetDevice(), vertexBuffer, nullptr);
+    vkFreeMemory(VulkanDevice::GetDevice(), vertexBufferMemory, nullptr);
+}
+
+//
+// Private
+//
+
+void BaseShape::CreateVertexBuffers()
+{
+    vertexCount = static_cast<uint32_t>(vertices.size());
+    assert(vertexCount >= 3 && "Vertex count must be at least 3 so that we can draw triangles ;)");
+
+    VkDeviceSize bufferSize = sizeof(vertices[0] * vertexCount);
+
+    VulkanDevice::CreateBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            vertexBuffer,
+            vertexBufferMemory
+    );
+
+    void *data;
+    vkMapMemory(VulkanDevice::GetDevice(), vertexBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(VulkanDevice::GetDevice(), vertexBufferMemory);
+}
--- src/engine/rendering/shapes/BaseShape.h
+++ src/engine/rendering/shapes/BaseShape.h
@@ -5,10 +5,26 @@
 #ifndef VULKAN_ENGINE_BASESHAPE_H
 #define VULKAN_ENGINE_BASESHAPE_H
 
+#include "../VulkanDevice.h"
+#include "Vertex.h"
+#include <vector>
 
 class BaseShape
 {
+public:
+    virtual void CreateShape();
+    virtual void Destroy();
 
+protected:
+    std::vector<Vertex> vertices = {};
+
+private:
+    void CreateVertexBuffers();
+
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+
+    uint32_t vertexCount;
 };
 
 
