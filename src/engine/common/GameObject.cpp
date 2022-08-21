ebc70ac5c5a7e1389776f124ef59c4cb5693246c
Diego Santos Seabra <dss-diego@hotmail.com>
2022-08-20 23:17:15 -0300

    changed game object to support position, scale and rotation

--- src/engine/common/GameObject.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Diego S. Seabra on 08/06/22.
-//
-
-#include "GameObject.h"
--- src/engine/common/GameObject.h
+++ src/engine/common/GameObject.h
@@ -5,20 +5,45 @@
 #ifndef VULKAN_ENGINE_GAMEOBJECT_H
 #define VULKAN_ENGINE_GAMEOBJECT_H
 
-#include <glm/vec2.hpp>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
 
-struct Transform {
-    glm::vec2 position;
-    uint32_t rotation; // REVIEW: In degrees? Euler angles? What is more intuitive?
-    glm::vec2 scale;
+#include <memory>
+#include "../rendering/shapes/Shape.h"
+#include "../rendering/shapes/Triangle.h"
+
+struct Transform2D {
+    glm::vec2 position{};
+    glm::vec2 scale{1.f, 1.f};
+    float rotation; // REVIEW: In degrees? Radians? Euler angles? What is more intuitive?
+
+    // 2D Transformation matrix (for rotationa and scale calculations)
+    glm::mat2 mat2() {
+        const float sine = glm::sin(rotation);
+        const float cosine = glm::cos(rotation);
+        glm::mat2 rotMatrix{{cosine, sine}, {-sine, cosine}};
+
+        glm::mat2 scaleMat{{scale.x, .0f}, {.0f, scale.y}};
+        return rotMatrix * scaleMat;
+    };
 };
 
 class GameObject
 {
 public:
-    virtual void Init() {};
-    virtual void Update() {};
-    virtual void Draw() {};
+    Transform2D transform{};
+    Shape shape{}; // TODO: Change this so that we can pass any shape we want ;)
+    glm::vec3 color{};
+
+    static GameObject Create() {
+        return GameObject{id++};
+    }
+
+private:
+    inline static uint32_t id = 0;
+
+    GameObject(uint32_t objId){ id = objId; }
 };
 
 
