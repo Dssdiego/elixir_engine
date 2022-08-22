8f4c6907ace1ab742d60ad2a879e1ca606ef595a
Diego Santos Seabra <dss-diego@hotmail.com>
2022-08-21 22:38:16 -0300

    fixing color blending
    blue and alpha channels weren't being taken into account

--- src/engine/rendering/VulkanPipeline.cpp
+++ src/engine/rendering/VulkanPipeline.cpp
@@ -250,7 +250,7 @@ void VulkanPipelineImpl::FillDefaultPipelineConfig()
             VK_BLEND_FACTOR_SRC_ALPHA,
             VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
             VK_BLEND_OP_SUBTRACT,
-            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
     };
 
     pipelineConfig.colorBlendInfo = {
